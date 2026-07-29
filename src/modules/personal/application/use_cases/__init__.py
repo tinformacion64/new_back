d6c57ee7a4e0f54797e96a4820c01b97c6c26449
JoseMaria443 <243752@ids.upchiapas.edu.mx>
2026-07-29 06:54:29 -0600

    Solucion al error en el despliegue

--- src/modules/personal/application/use_cases/__init__.py
+++ src/modules/personal/application/use_cases/__init__.py
@@ -4,9 +4,11 @@ Use cases del módulo de personal.
 from .login_empleado import LoginEmpleadoUseCase
 from .create_empleado import CreateEmpleadoUseCase
 from .update_empleado_estatus import UpdateEmpleadoEstatusUseCase
+from .change_password import ChangePasswordUseCase
 
 __all__ = [
     "LoginEmpleadoUseCase",
     "CreateEmpleadoUseCase",
     "UpdateEmpleadoEstatusUseCase",
+    "ChangePasswordUseCase",
 ]
